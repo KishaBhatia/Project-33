4bf9da616780dba33717c21d2871d2bfbe681c1b
KishaBhatia <64302865+KishaBhatia@users.noreply.github.com>
2020-06-06 14:46:25 +0530

    Update TargetChild.js

--- TargetChild.js
+++ TargetChild.js
@@ -1,7 +1,7 @@
 class TargetChild extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
-    this.image = loadImage("sprites/target.jpg");
+    this.image = loadImage("target.jpg");
     this.Visiblity = 255;
   }
 
